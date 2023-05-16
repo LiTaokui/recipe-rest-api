938915d2159f84ebe54338f34875f0b6af049aea
LI TAOKUI <1139224958@qq.com>
2023-05-16 10:02:15 +0800

    upgrade

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator func
+"""
+
+def add(x,y):
+    """add x and y and return result."""
+    return x+y
+
+def substract(x,y):
+    """substract x from y and return result"""
+    return y-x
